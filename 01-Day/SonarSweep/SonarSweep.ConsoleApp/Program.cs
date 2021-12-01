a2525fbe731c90a513242389460bb2115f43e736
xenxi <antoniom.diaz.moreno@gmail.com>
2021-12-01 20:50:54 +0100

    :lipstick: add console app

---
+++ 01-Day/SonarSweep/SonarSweep.ConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using SonarSweep;
+
+Print("### WELCOME TO: SINKING SPEED CALCULATOR 3000 ###", highlight: true);
+Wait();
+var report = File.ReadAllText("input.txt");
+Print(report);
+
+var increment = new SinkingSpeedCalculator().Calculate(report);
+Print($"RESULT: {increment}", highlight: true);
+
+static void Print(string text, bool highlight = false)
+{
+    Console.ResetColor();
+    if(highlight)
+        Console.ForegroundColor = ConsoleColor.Green;
+
+    Console.WriteLine(text);
+}
+
+static void Wait()
+{
+    Console.WriteLine(); 
+    Print("Press enter to continue...");
+    Console.ReadLine();
+}
