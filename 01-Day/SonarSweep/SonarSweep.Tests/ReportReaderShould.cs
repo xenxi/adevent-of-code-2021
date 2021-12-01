5013281ff1fce31a2b671c62f7e68c723cb8b450
xenxi <antoniom.diaz.moreno@gmail.com>
2021-12-01 19:44:55 +0100

    :white_check_mark: green test: return_empty_list_when_report_is_empty

---
+++ 01-Day/SonarSweep/SonarSweep.Tests/ReportReaderShould.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace SonarSweep.Tests
+{
+    [TestFixture]
+    public class ReportReaderShould
+    {
+        [Test]
+        public void return_empty_list_when_report_is_empty()
+        {
+            var reader = new ReportReader();
+            var aGivenEmptyReport = string.Empty;
+
+            var measurements = reader.ParseMeasurementsFrom(aGivenEmptyReport);
+            
+            measurements.Should().BeEmpty();
+        }
+
+    }
+}
--- 01-Day/SonarSweep/SonarSweep/ReportReader.cs
+++ 01-Day/SonarSweep/SonarSweep/ReportReader.cs
@@ -2,6 +2,11 @@
 {
     public class ReportReader
     {
-        public IList<int> ParseMeasurementsFrom(string report) => report.Split(' ').Select(strNum => (int.Parse(strNum))).ToList();
+        public IList<int> ParseMeasurementsFrom(string report)
+        {
+            if(report.Length == 0)
+                return new List<int>();
+            return report.Split(' ').Select(strNum => (int.Parse(strNum))).ToList();
+        }
     }
 }
