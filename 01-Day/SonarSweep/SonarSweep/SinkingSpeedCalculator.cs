b2e4dde694d79785e1d91264142e534447441576
xenxi <antoniom.diaz.moreno@gmail.com>
2021-12-01 18:42:03 +0100

    :recycle: refact extract method

--- 01-Day/SonarSweep/SonarSweep/SinkingSpeedCalculator.cs
+++ 01-Day/SonarSweep/SonarSweep/SinkingSpeedCalculator.cs
@@ -5,6 +5,11 @@
         public int CalculateIncrement(string report)
         {
             var measurements = ParseMeasurementsFrom(report);
+            return CalculeSpeed(measurements);
+        }
+
+        private static int CalculeSpeed(IList<int> measurements)
+        {
             var increments = 0;
             for (int i = 0; i < measurements.Count() - 1; i++)
             {
