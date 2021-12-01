4db61520f8d282c26eac48abe2fdef63d5912f6a
xenxi <antoniom.diaz.moreno@gmail.com>
2021-12-01 20:15:22 +0100

    :recycle: refact

--- 01-Day/SonarSweep/SonarSweep/ReportReader.cs
+++ 01-Day/SonarSweep/SonarSweep/ReportReader.cs
@@ -1,25 +1,13 @@
-﻿namespace SonarSweep
+﻿using System.Text.RegularExpressions;
+
+namespace SonarSweep
 {
     public class ReportReader
     {
         public IList<int> ParseMeasurementsFrom(string? report)
         {
-            if (report == null || report.Length == 0)
-                return new List<int>();
-
-            var strMeasurements = report.Split(' ');
-            return ReadNumbers(strMeasurements);
-        }
-
-        private static IList<int> ReadNumbers(string[] strMeasurements)
-        {
-            List<int> measurements = new List<int>();
-            foreach (var strMeasurement in strMeasurements)
-            {
-                if(int.TryParse(strMeasurement, out var measurement))
-                    measurements.Add(measurement);
-            }
-            return measurements;
+            var matches = Regex.Matches(report ?? String.Empty, @"\d+").Where(match => match.Success).Select(match => match.Value);
+            return matches.Select(match => int.Parse(match)).ToList();
         }
     }
 }
