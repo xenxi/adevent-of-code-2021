f33ea9c53852e1b60c3eb378a1e60901d0a17b10
xenxi <antoniom.diaz.moreno@gmail.com>
2021-12-01 18:00:10 +0100

    :test_tube: red test: calculate_depth_increments

---
+++ 01-Day/SonarSweep/SonarSweep.Tests/SinkingSpeedCalculatorFeature.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace SonarSweep.Tests
+{
+
+    [TestFixture]
+    public class SinkingSpeedCalculatorFeature
+    {
+        [Test]
+        public void calculate_depth_increments()
+        {
+            var calculator = new SinkingSpeedCalculator();
+            const string aGivenReport = "199 200 208 210 200 207 240 269 260 263 ";
+
+            var depthIncreases = calculator.CalculateIncrement(aGivenReport);
+
+            depthIncreases.Should().Be(7);
+        }
+    }
+}
---
+++ 01-Day/SonarSweep/SonarSweep/SinkingSpeedCalculator.cs
@@ -0,0 +1,14 @@
+﻿namespace SonarSweep
+{
+    public class SinkingSpeedCalculator
+    {
+        public SinkingSpeedCalculator()
+        {
+        }
+
+        public int CalculateIncrement(string aGivenReport)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
