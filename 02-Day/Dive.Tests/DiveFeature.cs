9299177646c19ceea0b078b81226552462e83d24
xenxi <antoniom.diaz.moreno@gmail.com>
2021-12-02 07:37:29 +0100

    :test_tube: red test: move

---
+++ 02-Day/Dive.Tests/DiveFeature.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Dive.Tests
+{
+    [TestFixture]
+    public class DiveFeature
+    {
+        [Test]
+        public void move()
+        {
+            var dive = new Dive();
+            var aGivenCommands = "forward 5\r\ndown 5\r\nforward 8\r\nup 3\r\ndown 8\r\nforward 2";
+            dive.ExecuteComands(aGivenCommands);
+
+            var locator = dive.BroadcastLocation();
+            
+            locator.Should().Be(150);
+        }
+
+    }
+}
---
+++ 02-Day/Dive/Dive.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dive
+{
+    public class Dive
+    {
+        public void ExecuteComands(string aGivenCommands)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int BroadcastLocation()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
