4106b7959a7d3c8019ba4efed9329f6cda906b12
xenxi <antoniom.diaz.moreno@gmail.com>
2021-12-02 07:20:56 +0100

    :tada: add day 2

---
+++ 02-Day/Dive.ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿Print("### WELCOME TO: DIVE 3000 ###", highlight: true);
+Wait();
+
+
+static void Print(string text, bool highlight = false)
+{
+    Console.ResetColor();
+    if (highlight)
+        Console.ForegroundColor = ConsoleColor.Blue;
+
+    Console.WriteLine(text);
+}
+
+static void Wait()
+{
+    Console.WriteLine();
+    Print("Press enter to continue...");
+    Console.ReadLine();
+}
