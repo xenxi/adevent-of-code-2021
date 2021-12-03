13bddab46c2366a4290a5282f198445f27741ff2
xenxi <antoniom.diaz.moreno@gmail.com>
2021-12-03 06:46:25 +0100

    :fire: remove duplicate tests

--- 02-Day/Dive.Tests/Commands/StringCommandRepositoryShould.cs
+++ 02-Day/Dive.Tests/Commands/StringCommandRepositoryShould.cs
@@ -7,26 +7,12 @@ namespace Dive.Tests.Commands
     [TestFixture]
     public class StringCommandRepositoryShould
     {
-        [Test]
-        public void return_empty_list_of_commands_when_input_is_emtpy()
+        [TestCase("")]
+        [TestCase(null)]
+        [TestCase("asdf tes 3")]
+        public void return_empty_list_of_commands_when_input_not_contains_any_valid_command(string input)
         {
-            var repository = new StringCommandRepository(string.Empty);
-
-            repository.GetAll().Should().BeEmpty();
-        }
-
-        [Test]
-        public void return_empty_list_of_commands_when_input_is_null()
-        {
-            var repository = new StringCommandRepository(null);
-
-            repository.GetAll().Should().BeEmpty();
-        }
-
-        [Test]
-        public void return_empty_list_of_commands_when_input_not_contains_any_valid_command()
-        {
-            var repository = new StringCommandRepository("asdf tes 3");
+            var repository = new StringCommandRepository(input);
 
             repository.GetAll().Should().BeEmpty();
         }
