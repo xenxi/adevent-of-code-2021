1ccdf97540a40c6a724cb7d8ef85d9558c128484
xenxi <antoniom.diaz.moreno@gmail.com>
2021-12-03 14:32:03 +0100

    :lipstick: print result

--- 02-Day/Dive.ConsoleApp/Program.cs
+++ 02-Day/Dive.ConsoleApp/Program.cs
@@ -1,7 +1,23 @@
-﻿Print("### WELCOME TO: DIVE 3000 ###", highlight: true);
+﻿using Dive;
+using Dive.Commands;
+using Dive.ConsoleApp;
+
+Print("### WELCOME TO: DIVE 3000 ###", highlight: true);
 Wait();
+var report = File.ReadAllText("input.txt");
+Print(report);
 
 
+var sender = BuildSender(report);
+
+sender.Send();
+
+static InstructionSender BuildSender(string commands)
+{
+    var repository = new StringCommandRepository(commands);
+    var notificator = new ConsoleLocatorNotificator((string text) => Print(text, true));
+    return new InstructionSender(repository, notificator);
+}
 static void Print(string text, bool highlight = false)
 {
     Console.ResetColor();
