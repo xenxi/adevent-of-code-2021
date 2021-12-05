fa6cad1a3f7e315ff9e75bff240aff53f876fddd
xenxi <antoniom.diaz.moreno@gmail.com>
2021-12-05 21:24:21 +0100

    :white_check_mark: green test: advanced_move

--- 02-Day/Dive.Tests/InstructionSenderFeature.cs
+++ 02-Day/Dive.Tests/InstructionSenderFeature.cs
@@ -21,5 +21,18 @@ namespace Dive.Tests
             notificator.Received(1).Notify(150);
         }
 
+        [Test]
+        public void advanced_move()
+        {
+            var aGivenDive = new AdvancedDive();
+            var aGivenCommands = "forward 5\r\ndown 5\r\nforward 8\r\nup 3\r\ndown 8\r\nforward 2";
+            var commandRepo = new StringCommandRepository(aGivenCommands);
+            var notificator = Substitute.For<LocatorNotificator>();
+            var sender = new InstructionSender(commandRepo, notificator);
+
+            sender.Send(aGivenDive);
+
+            notificator.Received(1).Notify(900);
+        }
     }
 }
