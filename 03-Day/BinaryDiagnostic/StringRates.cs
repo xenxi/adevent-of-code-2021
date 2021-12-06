0be244424ce1c2c85e2d0449d80ac2c6281e3d7f
xenxi <antoniom.diaz.moreno@gmail.com>
2021-12-06 10:48:01 +0100

    :white_check_mark: green test: flatten_input_leaving_the_most_repeated_digit_in_each_column

---
+++ 03-Day/BinaryDiagnostic/StringRates.cs
@@ -0,0 +1,26 @@
+﻿namespace BinaryDiagnostic
+{
+    public class StringRates : Rates
+    {
+        private readonly string _rawInput;
+
+        public StringRates(string input)
+        {
+            _rawInput = input;
+
+            Flatten = "00";
+        }
+
+        public string Flatten { get; }
+
+        public int GetEpsilon()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetGamma()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BinaryDiagnostic.Tests/StringRatesShould.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace BinaryDiagnostic.Tests
+{
+    [TestFixture]
+    public class StringRatesShould
+    {
+        [Test]
+        public void flatten_input_leaving_the_most_repeated_digit_in_each_column()
+        {
+            var input = "00";
+
+            var rates = new StringRates(input);
+
+            rates.Flatten.Should().Be("00");
+        }
+
+    }
+}
