d4f66b17738b0fe8ddb52fced2c51526f5b30b95
xenxi <antoniom.diaz.moreno@gmail.com>
2021-12-06 09:48:05 +0100

    :white_check_mark: green test: search_epsilon_rate

--- BinaryDiagnostic.Tests/HealthCheckerShould.cs
+++ BinaryDiagnostic.Tests/HealthCheckerShould.cs
@@ -18,5 +18,16 @@ namespace BinaryDiagnostic.Tests
             repository.Received(1).GetGamma();
         }
 
+        [Test]
+        public void search_epsilon_rate()
+        {
+            var repository = Substitute.For<Rates>();
+            var checker = new HealthChecker(repository);
+
+            checker.GenerateReport();
+
+            repository.Received(1).GetEpsilon();
+        }
+
     }
 }
