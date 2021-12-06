6bda26002d2960359f649fa10ee55a5fc0dd87d8
xenxi <antoniom.diaz.moreno@gmail.com>
2021-12-06 20:13:43 +0100

    :recycle: refact extract method

--- 03-Day/BinaryDiagnostic/HealthChecker.cs
+++ 03-Day/BinaryDiagnostic/HealthChecker.cs
@@ -11,15 +11,26 @@
 
         public HealthCheckerResponse GenerateReport()
         {
-            var gamma = _repository.GetGamma();
-            var epsilon = _repository.GetEpsilon();
-            var powerConsumption = gamma * epsilon;
+            var powerConsumption = CalculePowerConsumption();
+            var lifeSupport = CalculeLifeSupport();
 
+            return new HealthCheckerResponse(powerConsumption: powerConsumption, lifeSupport: lifeSupport);
+        }
+
+        private int CalculeLifeSupport()
+        {
             var oxygen = _repository.GetOxygenGenerator();
             var co2 = _repository.GetCo2Scrubber();
             var lifeSupport = oxygen * co2;
+            return lifeSupport;
+        }
 
-            return new HealthCheckerResponse(powerConsumption: powerConsumption, lifeSupport: lifeSupport);
+        private int CalculePowerConsumption()
+        {
+            var gamma = _repository.GetGamma();
+            var epsilon = _repository.GetEpsilon();
+            var powerConsumption = gamma * epsilon;
+            return powerConsumption;
         }
     }
 }
