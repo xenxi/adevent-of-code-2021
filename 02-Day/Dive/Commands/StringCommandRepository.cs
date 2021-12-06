928927eb65b0e24e1517f2aeae735e4a9a3d26fd
xenxi <antoniom.diaz.moreno@gmail.com>
2021-12-06 09:11:43 +0100

    :fire: remove unused code

--- 02-Day/Dive/Commands/StringCommandRepository.cs
+++ 02-Day/Dive/Commands/StringCommandRepository.cs
@@ -41,43 +41,10 @@ namespace Dive.Commands
             throw new InvalidOperationException("Invalid command");
         }
 
-        private void MatchDown(List<MoveCommandParam> moveCommandParams)
-        {
-            var match = Regex.Match(_rawData, @"(down(\s+)?\d+)", RegexOptions.IgnoreCase);
-
-            if (match.Success)
-            {
-                int step = ReadStep(match.Value);
-                moveCommandParams.Add(new MoveCommandParam(MoveCommandType.Down, step));
-            }
-        }
-
         private static int ReadStep(string input)
         {
             var match = Regex.Match(input, @"\d+");
             return int.Parse(match.Value);
-        }
-
-        private void MatchForward(List<MoveCommandParam> moveCommandParams)
-        {
-            var match = Regex.Match(_rawData, @"(forward(\s+)?\d+)", RegexOptions.IgnoreCase);
-
-            if (match.Success)
-            {
-                int step = ReadStep(match.Value);
-                moveCommandParams.Add(new MoveCommandParam(MoveCommandType.Forward, step));
-            }
-        }
-
-        private void MatchUp(List<MoveCommandParam> moveCommandParams)
-        {
-            var match = Regex.Match(_rawData, @"(up(\s+)?\d+)", RegexOptions.IgnoreCase);
-
-            if (match.Success)
-            {
-                int step = ReadStep(match.Value);
-                moveCommandParams.Add(new MoveCommandParam(MoveCommandType.Up, step));
-            }
         }
     }
 }
