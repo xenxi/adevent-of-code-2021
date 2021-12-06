428a9359b4262f503eeb548ebf6c220c61d155b4
xenxi <antoniom.diaz.moreno@gmail.com>
2021-12-06 19:59:31 +0100

    :fire: remove unecesary code

--- BinaryDiagnostic.Tests/HealthCheckerResponseMother.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BinaryDiagnostic.Tests
-{
-    public static class HealthCheckerResponseMother
-    {
-        public static HealthCheckerResponse Create(int powerConsumption, int lifeSupport) 
-            => new HealthCheckerResponse(powerConsumption: powerConsumption, lifeSupport: lifeSupport);
-    }
-}
