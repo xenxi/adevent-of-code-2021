91270d4c7d6e7779647f2aea12e07877f971dda4
xenxi <antoniom.diaz.moreno@gmail.com>
2021-12-06 19:51:55 +0100

    :recycle: refact renaming

--- BinaryDiagnostic.Tests/HealthCheckerFeature.cs
+++ BinaryDiagnostic.Tests/HealthCheckerFeature.cs
@@ -7,7 +7,7 @@ namespace BinaryDiagnostic.Tests
     public class HealthCheckerFeature
     {
         [Test]
-        public void generate_submarine_health_check_report()
+        public void generate_submarine_power_consumption_report()
         {
             const string input = "00100\r\n11110\r\n10110\r\n10111\r\n10101\r\n01111\r\n00111\r\n11100\r\n10000\r\n11001\r\n00010\r\n01010";
             var repository = new StringRates(input);
@@ -18,5 +18,6 @@ namespace BinaryDiagnostic.Tests
             var expectedReport = HealthCheckerResponseMother.Create(powerConsumption: 198);
             report.Should().BeEquivalentTo(expectedReport);
         }
+        
     }
 }
