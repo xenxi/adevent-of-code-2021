f9a0507f00a6a420b28d10e102319f664647a410
xenxi <antoniom.diaz.moreno@gmail.com>
2021-12-06 19:15:41 +0100

    :lipstick: add part 1

--- BinaryDiagnostic.ConsoleApp/Program.cs
+++ BinaryDiagnostic.ConsoleApp/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BinaryDiagnostic;
+
+Print("### WELCOME TO: HEALTH CHECKS 2001 ###", highlight: true);
+Wait();
+var ratesStr = File.ReadAllText("input.txt");
+Print(ratesStr);
+
+var repository = new StringRates(ratesStr);
+var cheker = new HealthChecker(repository);
+var report = cheker.GenerateReport();
+Print($"PowerConsumption: {report.PowerConsumption}");
+
+static void Print(string text, bool highlight = false)
+{
+    Console.ResetColor();
+    if (highlight)
+        Console.ForegroundColor = ConsoleColor.DarkMagenta;
+
+    Console.WriteLine(text);
+}
+
+
+static void Wait()
+{
+    Console.WriteLine();
+    Print("Press enter to continue...");
+    Console.ReadLine();
+}
