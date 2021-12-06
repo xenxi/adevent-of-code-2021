91918fc7d3c12c06fcd9fd883080c18098e85a8e
xenxi <antoniom.diaz.moreno@gmail.com>
2021-12-06 20:02:21 +0100

    :recycle: refact introduce field

--- BinaryDiagnostic.Tests/HealthCheckerFeature.cs
+++ BinaryDiagnostic.Tests/HealthCheckerFeature.cs
@@ -6,14 +6,19 @@ namespace BinaryDiagnostic.Tests
     [TestFixture]
     public class HealthCheckerFeature
     {
-        [Test]
-        public void generate_submarine_power_consumption_report()
+        private HealthChecker _cheker;
+        [SetUp]
+        public void SetUp()
         {
             const string input = "00100\r\n11110\r\n10110\r\n10111\r\n10101\r\n01111\r\n00111\r\n11100\r\n10000\r\n11001\r\n00010\r\n01010";
             var repository = new StringRates(input);
-            var cheker = new HealthChecker(repository);
-
-            var powerConsumption = cheker.GenerateReport().PowerConsumption;
+            _cheker = new HealthChecker(repository);
+        }
+            
+        [Test]
+        public void generate_submarine_power_consumption_report()
+        {
+            var powerConsumption = _cheker.GenerateReport().PowerConsumption;
 
             powerConsumption.Should().Be(198);
         }
@@ -21,11 +26,7 @@ namespace BinaryDiagnostic.Tests
         [Test]
         public void generate_submarine_life_support_report()
         {
-            const string input = "00100\r\n11110\r\n10110\r\n10111\r\n10101\r\n01111\r\n00111\r\n11100\r\n10000\r\n11001\r\n00010\r\n01010";
-            var repository = new StringRates(input);
-            var cheker = new HealthChecker(repository);
-
-            var lifeSupport = cheker.GenerateReport().LifeSupport;
+            var lifeSupport = _cheker.GenerateReport().LifeSupport;
 
             lifeSupport.Should().Be(230);
         }
