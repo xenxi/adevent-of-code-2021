3e19c7ed865a74e984067484976f4915571f8341
xenxi <antoniom.diaz.moreno@gmail.com>
2021-12-09 07:49:08 +0100

    :test_tube: red test: calcule_score_of_winner_board

---
+++ 04-Day/GiantSquid/BingoSolver.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GiantSquid
+{
+    public class BingoSolver
+    {
+        private string input;
+
+        public BingoSolver(string input)
+        {
+            this.input = input;
+        }
+
+        public int CalculeWinnerScore()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
