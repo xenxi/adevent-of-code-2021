322767b2e62ba49842e3f02e876fef2d1f229f5b
xenxi <antoniom.diaz.moreno@gmail.com>
2021-12-09 07:52:53 +0100

    :lipstick: add part 1

--- 04-Day/GiantSquid.ConsoleApp/Program.cs
+++ 04-Day/GiantSquid.ConsoleApp/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using GiantSquid;
+
+Print("### WELCOME TO: BINGO SOLVER 1999 ###", highlight: true);
+Wait();
+var input = File.ReadAllText("input.txt");
+Print(input);
+
+var bingoSolver = new BingoSolver(input);
+var winnerScore = bingoSolver.CalculeWinnerScore();
+Print($"Winner score: {winnerScore}");
+
+static void Print(string text, bool highlight = false)
+{
+    Console.ResetColor();
+    if (highlight)
+        Console.ForegroundColor = ConsoleColor.Red;
+
+    Console.WriteLine(text);
+}
+
+
+static void Wait()
+{
+    Console.WriteLine();
+    Print("Press enter to continue...");
+    Console.ReadLine();
+}
