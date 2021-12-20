74e4402d30591a975a7beb011cc51787e1afd10e
xenxi <antoniom.diaz.moreno@gmail.com>
2021-12-20 07:46:14 +0100

    :recycle: refact introduce fields

--- 04-Day/GiantSquid.Tests/BingoGameShould.cs
+++ 04-Day/GiantSquid.Tests/BingoGameShould.cs
@@ -7,11 +7,11 @@ namespace GiantSquid.Tests
     [TestFixture]
     public class BingoGameShould
     {
+        private BingoGame game;
+        private RandomNumberGenerator numberGenerator;
         [Test]
         public void add_board()
         {
-            var numberGenerator = Substitute.For<RandomNumberGenerator>();
-            var game = new BingoGame(numberGenerator);
             var aGivenBoard = GivenAnyBoard();
 
             game.AddBoard(aGivenBoard);
@@ -22,8 +22,6 @@ namespace GiantSquid.Tests
         [Test]
         public void return_winner_board()
         {
-            var numberGenerator = Substitute.For<RandomNumberGenerator>();
-            var game = new BingoGame(numberGenerator);
             var aGivenBoard = GivenAnyBoard();
             game.AddBoard(aGivenBoard);
 
@@ -32,6 +30,12 @@ namespace GiantSquid.Tests
             winner.Should().Be(aGivenBoard);
         }
 
+        [SetUp]
+        public void SetUp()
+        {
+            numberGenerator = Substitute.For<RandomNumberGenerator>();
+            game = new BingoGame(numberGenerator);
+        }
         private static Board GivenAnyBoard()
             => new Board(new int[,] { { 22, 13, 17, 11, 0 }, { 8, 2, 23, 4, 24 }, { 21, 9, 14, 16, 7 }, { 6, 10, 3, 18, 5 }, { 1, 12, 20, 15, 19 } });
     }
