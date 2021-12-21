6fed772ed3a45bb41e4d195daf08d1478030081f
xenxi <antoniom.diaz.moreno@gmail.com>
2021-12-21 07:57:55 +0100

    :white_check_mark: green test: return_second_board_as_the_winner

--- 04-Day/GiantSquid.Tests/BingoGameShould.cs
+++ 04-Day/GiantSquid.Tests/BingoGameShould.cs
@@ -1,6 +1,7 @@
 ﻿using FluentAssertions;
 using NSubstitute;
 using NUnit.Framework;
+using System.Linq;
 
 namespace GiantSquid.Tests
 {
@@ -29,6 +30,19 @@ namespace GiantSquid.Tests
 
             winner.Should().Be(aGivenBoard);
         }
+        [Test]
+        public void return_second_board_as_the_winner()
+        {
+            var firtsBoard = GivenAnyBoard();
+            var secondBoard = new Board(new int[,] { { 3, 15, 0, 2, 22 }, { 9, 18, 13, 17, 5 }, { 19, 8, 7, 25, 23 }, { 20, 11, 10, 24, 4 }, { 14, 21, 16, 12, 6 } });
+            game.AddBoard(firtsBoard);
+            game.AddBoard(secondBoard);
+            numberGenerator.Next().Returns(3, 15, 0, 2, 22);
+           
+            var winner = game.Play();
+
+            winner.Should().Be(secondBoard);
+        }
 
         [SetUp]
         public void SetUp()
@@ -36,6 +50,8 @@ namespace GiantSquid.Tests
             numberGenerator = Substitute.For<RandomNumberGenerator>();
             game = new BingoGame(numberGenerator);
         }
+        private static Board GivenAnOtherBoard()
+      => new Board(new int[,] { { 3, 15, 0, 2, 22 }, { 9, 18, 13, 17, 5 }, { 19, 8, 7, 25, 23 }, { 20, 11, 10, 24, 4 }, { 14, 21, 16, 12, 6 } });
         private static Board GivenAnyBoard()
             => new Board(new int[,] { { 22, 13, 17, 11, 0 }, { 8, 2, 23, 4, 24 }, { 21, 9, 14, 16, 7 }, { 6, 10, 3, 18, 5 }, { 1, 12, 20, 15, 19 } });
     }
--- 04-Day/GiantSquid/Board.cs
+++ 04-Day/GiantSquid/Board.cs
@@ -2,20 +2,38 @@
 
 public class Board
 {
-    private int[,] vs;
+    private readonly List<List<int>> lines;
+    private List<int> calledNumbers = new List<int>();
 
-    public Board(int[,] vs)
+    public Board(int[,] numbers)
     {
-        this.vs = vs;
-    }
+        this.lines = new List<List<int>>();
+        var numOfRows = numbers.GetLength(0);
+        var numOfColumns = numbers.GetLength(1);
+        for (int i = 0; i < numOfRows; i++)
+        {
+            var line = new List<int>();
+            for (int j = 0; j < numOfColumns; j++)
+                line.Add(numbers[i, j]);
 
-    public void Play(int number)
-    {
-        
+            lines.Add(line);
+        }
     }
 
     public bool Bingo()
     {
-        return true;
+        foreach (var line in lines)
+        {
+            if (line.All(number => calledNumbers.Contains(number)))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void Play(int number)
+    {
+        calledNumbers.Add(number);
     }
 }
