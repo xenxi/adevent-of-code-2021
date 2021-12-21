8831354aab7410ca0fba8cf0866ada4276e23f03
xenxi <antoniom.diaz.moreno@gmail.com>
2021-12-21 08:08:44 +0100

    :fire: remove unecesary code

--- 04-Day/GiantSquid.Tests/BingoGameShould.cs
+++ 04-Day/GiantSquid.Tests/BingoGameShould.cs
@@ -1,7 +1,6 @@
 ﻿using FluentAssertions;
 using NSubstitute;
 using NUnit.Framework;
-using System.Linq;
 
 namespace GiantSquid.Tests
 {
@@ -51,8 +50,7 @@ namespace GiantSquid.Tests
             numberGenerator = Substitute.For<RandomNumberGenerator>();
             game = new BingoGame(numberGenerator);
         }
-        private static Board GivenAnOtherBoard()
-      => new Board(new int[,] { { 3, 15, 0, 2, 22 }, { 9, 18, 13, 17, 5 }, { 19, 8, 7, 25, 23 }, { 20, 11, 10, 24, 4 }, { 14, 21, 16, 12, 6 } });
+
         private static Board GivenAnyBoard()
             => new Board(new int[,] { { 22, 13, 17, 11, 0 }, { 8, 2, 23, 4, 24 }, { 21, 9, 14, 16, 7 }, { 6, 10, 3, 18, 5 }, { 1, 12, 20, 15, 19 } });
     }
