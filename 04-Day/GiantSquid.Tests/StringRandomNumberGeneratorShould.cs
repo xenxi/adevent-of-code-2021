06450ad3af9205bbdec97a2fa4b684c51c07e3c6
xenxi <antoniom.diaz.moreno@gmail.com>
2021-12-22 07:59:06 +0100

    :white_check_mark: green test: return_number_given_in_string_with_one_number

---
+++ 04-Day/GiantSquid.Tests/StringRandomNumberGeneratorShould.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace GiantSquid.Tests
+{
+    public class StringRandomNumberGeneratorShould
+    {
+        [TestCase("1", 1)]
+        [TestCase("2", 2)]
+        [TestCase("5", 5)]
+        public void return_number_given_in_string_with_one_number(string input, int expectedNumber)
+        {
+            var generator = new StringRandomNumberGenerator(input);
+
+            var number = generator.Next();
+
+            number.Should().Be(expectedNumber);
+        }
+
+    }
+}
---
+++ 04-Day/GiantSquid/StringRandomNumberGenerator.cs
@@ -0,0 +1,17 @@
+﻿namespace GiantSquid
+{
+    public class StringRandomNumberGenerator : RandomNumberGenerator
+    {
+        private string input;
+
+        public StringRandomNumberGenerator(string input)
+        {
+            this.input = input;
+        }
+
+        public int? Next()
+        {
+            return int.Parse(input);
+        }
+    }
+}
