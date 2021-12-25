5a35c849f4952124484ab4b6bd18a6b4e8016087
xenxi <antoniom.diaz.moreno@gmail.com>
2021-12-25 13:31:25 +0100

    :white_check_mark: green test: return_a_board_ignore_rolls

--- 04-Day/GiantSquid/StringBoardRepository.cs
+++ 04-Day/GiantSquid/StringBoardRepository.cs
@@ -1,39 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace GiantSquid;
 
-namespace GiantSquid
+public class StringBoardRepository
 {
-    public class StringBoardRepository
+    private string input;
+
+    public StringBoardRepository(string input)
     {
-        private string input;
+        this.input = input;
+    }
 
-        public StringBoardRepository(string input)
+    public IList<Board> GetAll()
+    {
+        var strLines = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+        var lines = new List<List<int>>();
+        foreach (var line in strLines)
         {
-            this.input = input;
-        }
-
-        public IList<Board> GetAll()
-        {
-            var strLines = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
-            var lines = new List<List<int>>();
-            foreach (var line in strLines)
-            {
-                var strNumbersInLine = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            var strNumbersInLine = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            if (strNumbersInLine.Count() == 5)
                 lines.Add(strNumbersInLine.Select(x => int.Parse(x)).ToList());
-            }
-            var boards = new List<Board>();
-            var group = lines.Take(5);
-
-            while (group.Any())
-            {
-                boards.Add(new Board(group.ToList()));
-                group = lines.Skip(5 * boards.Count).Take(5);
-            }
-
-            return boards;
         }
+        var boards = new List<Board>();
+        var group = lines.Take(5);
+
+        while (group.Any())
+        {
+            boards.Add(new Board(group.ToList()));
+            group = lines.Skip(5 * boards.Count).Take(5);
+        }
+
+        return boards;
     }
 }
